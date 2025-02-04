efd028c9d4edf06f2a096bc5c4989afe45103056
Jiamin Ng <jiamin.ng@u.nus.edu>
2025-02-04 09:37:38 +0800

    Update

--- obj/Release/net8.0/WebApi.AssemblyInfo.cs
+++ obj/Release/net8.0/WebApi.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5beb5d8c72a3cb1062aa131a0c59368a0fec7ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
